114b53f4cbe6f6ba4aaad96687af2f67a8944b68
Gunji Kamiya <kamiyagunji@icloud.com>
2020-05-30 01:24:10 +0900

    delete comment

--- src/components/add-button/index.tsx
+++ src/components/add-button/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-// I put the buoon in absolute position because I want it to be on top of everything behind it
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'navy',
